6a2020c83b0cb739126b06262257f36cdca4c96a
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-07-31 10:47:04 -0700

    Add validation to user form

--- controllers/usersController.js.js
+++ controllers/usersController.js.js
@@ -2,6 +2,41 @@
 const asyncHandler = require("express-async-handler");
 const usersStorage = require("../storages/usersStorage");
 
+// ***********************************************************************************************************************
+// Next up we're going to add validation to our form
+const { body, validationResult } = require("express-validator");
+
+const alphaErr = "must only contain letters.";
+const lengthErr = "must be between 1 and 10 characters.";
+
+const validateUser = [
+  body("firstName").trim()
+    .isAlpha().withMessage(`First name ${alphaErr}`)
+    .isLength({ min: 1, max: 10 }).withMessage(`First name ${lengthErr}`),
+  body("lastName").trim()
+    .isAlpha().withMessage(`Last name ${alphaErr}`)
+    .isLength({ min: 1, max: 10 }).withMessage(`Last name ${lengthErr}`),
+];
+
+// We can pass an entire array of middleware validations to our controller.
+exports.usersCreatePost = [
+  validateUser,
+  asyncHandler(async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).render("users", {
+        title: "User List",
+        errors: errors.array(),
+      });
+    }
+    const { firstName, lastName } = req.body;
+    usersStorage.addUser({firstName, lastName});
+    res.redirect("/");
+  })
+];
+// ***********************************************************************************************************************
+
+
 exports.usersCreateGet = asyncHandler(async (req, res) => {
   res.render("users", {
     title: "User List",
